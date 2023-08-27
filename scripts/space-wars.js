35cb330c4494e4cf366a775b560573955f07791f
Vladyslav Zaikovskyi <vladyslav.zaikovskyi@gmail.com>
2023-08-28 02:18:28 +0300

    modified

--- scripts/space-wars.js
+++ scripts/space-wars.js
@@ -68,13 +68,13 @@ function createSpaceWars() {
 
     //load images
     shipImg = new Image();
-    shipImg.src = shipImage;
+    shipImg.src = "../img/player.png";
     shipImg.onload = function (params) {
       context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     };
 
     alienImg = new Image();
-    alienImg.src = alienImage;
+    alienImg.src = "../img/alien.png";
     createAliens();
 
     requestAnimationFrame(update);
